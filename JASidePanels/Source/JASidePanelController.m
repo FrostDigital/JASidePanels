036a5eaad99693fde84dd61717286346939ec153
JosefinAndersson <josefin@frostdigital.se>
2015-10-20 14:10:38 +0200

    Makes shadow on center panel smaller

--- JASidePanels/Source/JASidePanelController.m
+++ JASidePanels/Source/JASidePanelController.m
@@ -290,7 +290,7 @@ static char ja_kvoContext;
     }
     container.layer.shadowPath = shadowPath.CGPath;
     container.layer.shadowColor = [UIColor blackColor].CGColor;
-    container.layer.shadowRadius = 10.0f;
+    container.layer.shadowRadius = 4.0f;
     container.layer.shadowOpacity = 0.75f;
     container.clipsToBounds = NO;
 }
